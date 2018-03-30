bf71607c9b10ba59edb79660e3a270c5fa4e8cac
Marek Jurkiewicz <mjurkiewicz@consdata.pl>
2018-03-30 13:06:30 +0200

    Add X509TrustManager to http client

--- src/main/java/org/bitsofinfo/hazelcast/discovery/consul/ConsulClientBuilder.java
+++ src/main/java/org/bitsofinfo/hazelcast/discovery/consul/ConsulClientBuilder.java
@@ -1,24 +1,28 @@
 package org.bitsofinfo.hazelcast.discovery.consul;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-
 import com.google.common.net.HostAndPort;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.orbitz.consul.Consul;
 import com.orbitz.consul.Consul.Builder;
 import org.apache.commons.codec.binary.Base64;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 /**
  * An implementation of a Consul client builder.
  * 
@@ -54,8 +58,9 @@ public class ConsulClientBuilder implements ConsulBuilder {
 				
 				if ( (consulSslServerCertFilePath != null && !consulSslServerCertFilePath.trim().isEmpty()) || 
 						(consulSslServerCertBase64 != null && !consulSslServerCertBase64.trim().isEmpty()) ) {
-					
-					consulBuilder.withSslContext(getSSLContext(consulSslServerCertFilePath, consulSslServerCertBase64));
+					TrustManager[] trustManagers = createTrustedManager(consulSslServerCertFilePath, consulSslServerCertBase64);
+					consulBuilder.withSslContext(getSSLContext(trustManagers));
+					consulBuilder.withTrustManager(getTrustedManager(trustManagers));
 				}
 				
 				if (!consulServerHostnameVerify) {
@@ -86,41 +91,33 @@ public class ConsulClientBuilder implements ConsulBuilder {
 	}
 
 	/**
+	 * Choose X509TrustManager from Trusted Managers
+	 * @param trustManagers
+	 * @return
+	 */
+	private X509TrustManager getTrustedManager(TrustManager[] trustManagers) {
+		X509TrustManager x509Tm = null;
+		for (TrustManager tm : trustManagers) {
+			if (tm instanceof X509TrustManager) {
+				x509Tm = (X509TrustManager) tm;
+				break;
+			}
+		}
+		return x509Tm;
+	}
+
+	/**
 	 * Method to build an SSL context to be used by the consul builder
 	 * 
-	 * @param consulSslServerCertFilePath
-	 * @param consulSslServerCertBase64
+	 * @param trustManagers
 	 * @return SSLContext object
 	 * @throws Exception
 	 */
-	private SSLContext getSSLContext(String	consulSslServerCertFilePath, String consulSslServerCertBase64) throws Exception{
+	private SSLContext getSSLContext(TrustManager[] trustManagers) throws Exception{
 		
 		try{
-			
-			InputStream is = null;
-			
-			//Get the self signed cert either from file or base64 encoded string passed through xml config
-			if (consulSslServerCertFilePath !=null && !consulSslServerCertFilePath.trim().isEmpty()) {
-				is = new FileInputStream(consulSslServerCertFilePath);
-			} else {
-				is = new ByteArrayInputStream(Base64.decodeBase64(consulSslServerCertBase64));
-			}
-					
-			CertificateFactory cf = CertificateFactory.getInstance("X.509");
-			X509Certificate caCert = (X509Certificate)cf.generateCertificate(is);
-	
-			TrustManagerFactory tmf = TrustManagerFactory
-			    .getInstance(TrustManagerFactory.getDefaultAlgorithm());
-			
-			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-			ks.load(null); // We don't need the KeyStore instance to come from a file.
-			ks.setCertificateEntry("caCert", caCert);
-	
-			tmf.init(ks);
-			TrustManager tm[] =tmf.getTrustManagers();
-	
 			SSLContext sslContext = SSLContext.getInstance("TLSv1");
-			sslContext.init(null, tm, null);
+			sslContext.init(null, trustManagers, null);
 			
 			return sslContext;
 			
@@ -131,6 +128,30 @@ public class ConsulClientBuilder implements ConsulBuilder {
 		}
 		
 	}
-	
-	
+
+	private TrustManager[] createTrustedManager(String consulSslServerCertFilePath, String consulSslServerCertBase64) throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+		InputStream is = null;
+
+		//Get the self signed cert either from file or base64 encoded string passed through xml config
+		if (consulSslServerCertFilePath !=null && !consulSslServerCertFilePath.trim().isEmpty()) {
+			is = new FileInputStream(consulSslServerCertFilePath);
+		} else {
+			is = new ByteArrayInputStream(Base64.decodeBase64(consulSslServerCertBase64));
+		}
+
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		X509Certificate caCert = (X509Certificate)cf.generateCertificate(is);
+
+		TrustManagerFactory tmf = TrustManagerFactory
+		    .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		ks.load(null); // We don't need the KeyStore instance to come from a file.
+		ks.setCertificateEntry("caCert", caCert);
+
+		tmf.init(ks);
+		return tmf.getTrustManagers();
+	}
+
+
 }
